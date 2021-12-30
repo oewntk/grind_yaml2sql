7774f5d95553a4296c86e2a30c5200e7589498d5
1313ou <1313ou@gmail.com>
2021-12-30 09:58:10 +0100

    extra entry

--- src/main/java/org/oewntk/grind/yaml2sql/Mapper.java
+++ src/main/java/org/oewntk/grind/yaml2sql/Mapper.java
@@ -4,44 +4,15 @@
 
 package org.oewntk.grind.yaml2sql;
 
-import org.oewntk.model.CoreModel;
 import org.oewntk.model.Model;
 import org.oewntk.sql.out.NIDMaps;
-import org.oewntk.sql.out.Names;
 import org.oewntk.yaml.in.Factory;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
 
 public class Mapper
 {
-	static public void print(final CoreModel model, final File outDir) throws IOException
-	{
-		try (PrintStream ps = new PrintStream(new FileOutputStream(new File(outDir, Names.WORDS.FILE)), true, StandardCharsets.UTF_8))
-		{
-			NIDMaps.printWords(ps, model.lexes);
-		}
-		try (PrintStream ps = new PrintStream(new FileOutputStream(new File(outDir, Names.CASEDWORDS.FILE)), true, StandardCharsets.UTF_8))
-		{
-			NIDMaps.printCasedWords(ps, model.lexes);
-		}
-		try (PrintStream ps = new PrintStream(new FileOutputStream(new File(outDir, Names.MORPHS.FILE)), true, StandardCharsets.UTF_8))
-		{
-			NIDMaps.printMorphs(ps, model.lexes);
-		}
-		try (PrintStream ps = new PrintStream(new FileOutputStream(new File(outDir, Names.PRONUNCIATIONS.FILE)), true, StandardCharsets.UTF_8))
-		{
-			NIDMaps.printPronunciations(ps, model.lexes);
-		}
-		try (PrintStream ps = new PrintStream(new FileOutputStream(new File(outDir, Names.SYNSETS.FILE)), true, StandardCharsets.UTF_8))
-		{
-			NIDMaps.printSynsets(ps, model.synsets);
-		}
-	}
-
 	static public void main(String[] args) throws IOException
 	{
 		File outDir = new File(args[2]);
@@ -52,6 +23,6 @@ public class Mapper
 		}
 
 		final Model model = Factory.makeModel(args);
-		print(model, outDir);
+		NIDMaps.printMaps(model, outDir);
 	}
 }
