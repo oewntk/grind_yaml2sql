51eedcb08b01c0c7f575c9442a3fec4eed553552
1313ou <1313ou@gmail.com>
2024-04-29 08:53:04 +0200

    fix key tests

--- src/test/java/org/oewntk/grind/yaml2sql/TestsYamlNIDMap.kt
+++ src/test/java/org/oewntk/grind/yaml2sql/TestsYamlNIDMap.kt
@@ -10,41 +10,56 @@ import org.oewntk.grind.yaml2sql.LibTestsYamlCommon.model
 import org.oewntk.model.Key
 import org.oewntk.model.KeyF
 import org.oewntk.model.Lex
+import org.oewntk.sql.out.Lexes.makeLexesNIDs
 import org.oewntk.sql.out.NIDMaps
 import kotlin.test.assertEquals
 
 class TestsYamlNIDMap {
 
-	@Test
-	fun testLookupByKey() {
-		val lex = Lex("C", "n", null)
-		val k = Key.W_P_A.of_t(lex)
-		NIDMaps.lookup(lexKeyToNIDByKey, k)
-		val k2 = Key.W_P_A.of_t(lex)
-		val r = NIDMaps.lookup(lexKeyToNIDByKey, k2)
-		assertEquals(r, 1)
+	// first
+	val hoodLex = Lex("'hood", "n", null)
+
+	// last
+	val zymurgyLex = Lex("zymurgy", "n", null)
+
+	private fun testLookupByKey(lex: Lex, expectedNID: Int) {
+		val lexK = Key.W_P_A.of(lex, Lex::lemma, Lex::type)
+		val r1 = NIDMaps.lookup(lexKeyToNIDByKey, lexK)
+		assertEquals(expectedNID, r1)
+
+		val lexK2 = Key.W_P_A.of_t(lex)
+		val r2 = NIDMaps.lookup(lexKeyToNIDByKey, lexK2)
+		assertEquals(expectedNID, r2)
 	}
 
-	@Test(expected = NullPointerException::class)
-	fun failingTestLookupByKey() {
-		val lex = Lex("C", "n", null)
-		val k = Key.W_P_A.of_t(lex)
-		NIDMaps.lookup(lexKeyToNIDByKey, k)
-		val k2 = Key.W_P_A.of_t(lex)
-		NIDMaps.lookup(lexKeyToNIDByKeyF, k2)
+	private fun testLookupByKeyF(lex: Lex, expectedNID: Int) {
+		val lexK = KeyF.F_W_P_A.Mono.of(Lex::lemma, Lex::type, lex)
+		val r = NIDMaps.lookup(lexKeyToNIDByKeyF, lexK)
+		assertEquals(expectedNID, r)
+	}
+
+	@Test
+	fun testLookupByKey() {
+		testLookupByKey(hoodLex, 1) // first
+		testLookupByKey(zymurgyLex, lexKeyToNIDByKey.size) // last
 	}
 
 	@Test
 	fun testLookupByKeyF() {
-		val lex = Lex("C", "n", null)
-		val k = KeyF.F_W_P_A.Mono.of(Lex::lemma, Lex::type, lex)
-		val r = NIDMaps.lookup(lexKeyToNIDByKeyF, k)
-		assertEquals(r, 1)
+		testLookupByKeyF(hoodLex, 1) // first
+		testLookupByKeyF(zymurgyLex, lexKeyToNIDByKeyF.size) // last
+	}
+
+	@Test(expected = NullPointerException::class)
+	fun failingTestLookupByKey() {
+		val lex = hoodLex
+		val k = Key.W_P_A.of_t(lex)
+		NIDMaps.lookup(lexKeyToNIDByKeyF, k)
 	}
 
 	@Test(expected = NullPointerException::class)
 	fun failingTestLookupByKeyF() {
-		val lex = Lex("C", "n", null)
+		val lex = hoodLex
 		val k = KeyF.F_W_P_A.Mono.of(Lex::lemma, Lex::type, lex)
 		NIDMaps.lookup(lexKeyToNIDByKey, k)
 	}
@@ -61,16 +76,13 @@ class TestsYamlNIDMap {
 			checkNotNull(model)
 
 			// lex key to NID
-			lexKeyToNIDByKey = model!!.lexes
-				.asSequence()
-				.map { Key.W_P_A.of_t(it) }
-				.withIndex()
-				.associate { it.value to it.index + 1 } // map(of_t(lex), nid)
+			lexKeyToNIDByKey = makeLexesNIDs(model!!.lexes)
 
 			// lex keyf to NID
 			lexKeyToNIDByKeyF = model!!.lexes
 				.asSequence()
 				.map { KeyF.F_W_P_A.Mono.of(Lex::lemma, Lex::type, it) }
+				.sorted()
 				.withIndex()
 				.associate { it.value to it.index + 1 } // map(of_t(lex), nid)
 		}
