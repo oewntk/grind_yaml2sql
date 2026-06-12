0cdd4bf13cc9fa5980633fb7f6e14fd3f960c42c
1313ou <1313ou@gmail.com>
2026-06-12 11:50:35 +0200

    generate schema and sources with model consumers

--- src/main/java/org/oewntk/grind/yaml2sql/Grind.kt
+++ src/main/java/org/oewntk/grind/yaml2sql/Grind.kt
@@ -33,14 +33,15 @@ object Grind {
         val parser = ArgParser("yaml2sql")
         // Options (start with - or --)
         // @formatter:off
-        val in1 by parser.argument(     ArgType.String,                                                    description = "Input dir or file")
-        val in2 by parser.argument(     ArgType.String,                                                    description = "Extra input dir or file")
-        val out by parser.argument(     ArgType.String,                                                    description = "Output dir or file")
-        val verbose by parser.option(   ArgType.Boolean,       shortName = "v",  fullName = "verbose",     description = "Verbose output")             .default(false)
+        val in1 by parser.argument(     ArgType.String,                                              description = "Input dir or file")
+        val in2 by parser.argument(     ArgType.String,                                              description = "Extra input dir or file")
+        val out by parser.argument(     ArgType.String,                                              description = "Output dir or file")
+        val verbose by parser.option(   ArgType.Boolean, shortName = "v",  fullName = "verbose",     description = "Verbose output")           .default(false)
 
-        val traceTime by parser.option( ArgType.Boolean,       shortName = "tt", fullName = "trace:time",  description = "trace time")                 .default(false)
-        val traceHeap by parser.option( ArgType.Boolean,       shortName = "th", fullName = "trace:heap",  description = "trace heap")                 .default(false)
+        val traceTime by parser.option( ArgType.Boolean, shortName = "tt", fullName = "trace:time",  description = "trace time")               .default(false)
+        val traceHeap by parser.option( ArgType.Boolean, shortName = "th", fullName = "trace:heap",  description = "trace heap")               .default(false)
         // @formatter:on
+        parser.parse(args)
 
         // Tracing
         Tracing.traceTime = traceTime
@@ -64,16 +65,16 @@ object Grind {
         Tracing.psInfo.println("[Output] " + outDir.absolutePath)
 
         // Supply model
-        progress("before model is supplied", startTime)
+        progress("before model is supplied", startTime, verbose = verbose)
         val model = Factory(inDir, inDir2, verbose = verbose).get()
-        progress("after model is supplied", startTime)
+        progress("after model is supplied", startTime, verbose = verbose)
 
         // Consume model
-        progress("before model is consumed", startTime)
+        progress("before model is consumed", startTime, verbose = verbose)
         ModelConsumer(outDir, verbose = verbose).accept(model!!)
-        progress("after model is consumed", startTime)
+        progress("after model is consumed", startTime, verbose = verbose)
 
         // End
-        progress("total", startTime)
+        progress("total", startTime, verbose = verbose)
     }
 }
