545eb70fac5041002bab96eccaac813f53b64e28
1313ou <1313ou@gmail.com>
2026-07-21 07:39:37 +0200

    unnecessary if

--- src/test/java/org/oewntk/grind/yaml2sql/LibTestsYamlCommon.kt
+++ src/test/java/org/oewntk/grind/yaml2sql/LibTestsYamlCommon.kt
@@ -14,8 +14,7 @@ object LibTestsYamlCommon {
 
     private val source: String? = System.getProperty("SOURCE")
 
-    val silent = if (System.getProperties().containsKey("VERBOSE")) false
-    else if (System.getProperties().containsKey("SILENT")) true
+    val silent = !System.getProperties().containsKey("VERBOSE") && if (System.getProperties().containsKey("SILENT")) true
     else true
 
     val ps: PrintStream = if (!silent) Tracing.psInfo else Tracing.psNull
