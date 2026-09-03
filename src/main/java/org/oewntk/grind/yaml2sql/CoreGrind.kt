6501817415b55ee6344227d392681bc3ddacf6fb
1313ou <1313ou@gmail.com>
2026-09-03 11:24:10 +0200

    skip inverses flag

--- src/main/java/org/oewntk/grind/yaml2sql/CoreGrind.kt
+++ src/main/java/org/oewntk/grind/yaml2sql/CoreGrind.kt
@@ -39,9 +39,16 @@ object CoreGrind {
         val compat by parser.option(     ArgType.Boolean,  shortName = "c",  fullName = "compat",      description = "Compat schema")            .default(false)
         val doNotThrow by parser.option( ArgType.Boolean,  shortName = "nt", fullName = "no_throw",    description = "Do not throw")             .default(false)
         val verbose by parser.option(    ArgType.Boolean,  shortName = "v",  fullName = "verbose",     description = "Verbose output")           .default(false)
+        val in1 by parser.argument(       ArgType.String,                                               description = "Input dir or file")
+        val in2 by parser.argument(       ArgType.String,                                               description = "Extra input dir or file")
+        val out by parser.argument(       ArgType.String,                                               description = "Output dir or file")
+        val compat by parser.option(      ArgType.Boolean,  shortName = "c",  fullName = "compat",      description = "Compat schema")              .default(false)
+        val skipInverses by parser.option(ArgType.Boolean, shortName = "ni", fullName = "skip inverses", description = "Skip inverse relations")   .default(false)
+        val doNotThrow by parser.option(  ArgType.Boolean,  shortName = "nt", fullName = "no_throw",    description = "Do not throw")               .default(false)
+        val verbose by parser.option(     ArgType.Boolean,  shortName = "v",  fullName = "verbose",     description = "Verbose output")             .default(false)
 
-        val traceTime by parser.option(  ArgType.Boolean,  shortName = "tt", fullName = "trace:time",  description = "trace time")               .default(false)
-        val traceHeap by parser.option(  ArgType.Boolean,  shortName = "th", fullName = "trace:heap",  description = "trace heap")               .default(false)
+        val traceTime by parser.option(   ArgType.Boolean,  shortName = "tt", fullName = "trace:time",  description = "trace time")                 .default(false)
+        val traceHeap by parser.option(   ArgType.Boolean,  shortName = "th", fullName = "trace:heap",  description = "trace heap")                 .default(false)
         // @formatter:on
 
         // Tracing
@@ -67,13 +74,12 @@ object CoreGrind {
 
         // Supply model
         progress("before model is supplied", startTime)
-        val model = CoreFactory(inDir, throws = !doNotThrow, verbose = verbose).get()
-        //Tracing.psInfo.printf("[CoreModel] %s%n%s%n%n", model.getSource(), model.info());
+        val model = CoreFactory(inDir, inverses = inverses, throws = !doNotThrow, verbose = verbose).get()
         progress("after model is supplied", startTime)
 
         // Consume model
         progress("before model is consumed", startTime)
-        CoreModelConsumer(outDir, compatSchema = compat, verbose = verbose).accept(model!!)
+        CoreModelConsumer(outDir, compatSchema = compat, skipInverses = skipInverses, verbose = verbose).accept(model!!)
         progress("after model is consumed", startTime)
 
         // End
--- src/main/java/org/oewntk/grind/yaml2sql/Grind.kt
+++ src/main/java/org/oewntk/grind/yaml2sql/Grind.kt
@@ -38,9 +38,16 @@ object Grind {
         val out by parser.argument(     ArgType.String,                                              description = "Output dir or file")
         val compat by parser.option(    ArgType.Boolean, shortName = "c",  fullName = "compat",      description = "Compat schema")            .default(false)
         val verbose by parser.option(   ArgType.Boolean, shortName = "v",  fullName = "verbose",     description = "Verbose output")           .default(false)
+        val in1 by parser.argument(       ArgType.String,                                                description = "Input dir or file")
+        val in2 by parser.argument(       ArgType.String,                                                description = "Extra input dir or file")
+        val out by parser.argument(       ArgType.String,                                                description = "Output dir or file")
+        val compat by parser.option(      ArgType.Boolean, shortName = "c",  fullName = "compat",        description = "Compat schema")              .default(false)
+        val skipInverses by parser.option(ArgType.Boolean, shortName = "ni", fullName = "skip inverses", description = "Skip inverse relations")     .default(false)
+        val doNotThrow by parser.option(  ArgType.Boolean, shortName = "nt", fullName = "no_throw",      description = "Do not throw")               .default(false)
+        val verbose by parser.option(     ArgType.Boolean, shortName = "v",  fullName = "verbose",       description = "Verbose output")             .default(false)
 
-        val traceTime by parser.option( ArgType.Boolean, shortName = "tt", fullName = "trace:time",  description = "trace time")               .default(false)
-        val traceHeap by parser.option( ArgType.Boolean, shortName = "th", fullName = "trace:heap",  description = "trace heap")               .default(false)
+        val traceTime by parser.option(   ArgType.Boolean, shortName = "tt", fullName = "trace:time",    description = "trace time")                 .default(false)
+        val traceHeap by parser.option(   ArgType.Boolean, shortName = "th", fullName = "trace:heap",    description = "trace heap")                 .default(false)
         // @formatter:on
         parser.parse(args)
 
@@ -67,12 +74,12 @@ object Grind {
 
         // Supply model
         progress("before model is supplied", startTime, verbose = verbose)
-        val model = Factory(inDir, inDir2, verbose = verbose).get()
+        val model = Factory(inDir, inDir2, inverses = inverses, throws = !doNotThrow, verbose = verbose).get()
         progress("after model is supplied", startTime, verbose = verbose)
 
         // Consume model
         progress("before model is consumed", startTime, verbose = verbose)
-        ModelConsumer(outDir, compatSchema = compat, verbose = verbose).accept(model!!)
+        ModelConsumer(outDir, compatSchema = compat, skipInverses = skipInverses, verbose = verbose).accept(model!!)
         progress("after model is consumed", startTime, verbose = verbose)
 
         // End
